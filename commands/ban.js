f78d8083eeb0209cc1fecf4d1d1b04f2a511eb13
bruh bruh <77313994+psychic1pikachu@users.noreply.github.com>
2021-06-26 12:32:48 +0000

    episode 3

--- commands/ban.js
+++ commands/ban.js
@@ -1,27 +1,17 @@
-const Discord = require("discord.js");
-const { MessageEmbed } = require("discord.js");
+const Discord = require('discord.js')
 
 module.exports = {
     name: "ban",
-    description: "ban ppl lol",
+    description: ":/",
 
     async run (client, message, args) {
-      //first we are going to see if the user has perms
-      if(!message.member.hasPermission("BAN_MEMBERS")) return;
-      if(!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send('I don\'t have perms to use this command');// here we are going to check if the bot has perms to ban ppl
+      if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send(':/ you dont have perms to ban this member bruhhhh');
 
-      const personToban = message.mentions.members.first() || message.guild.members.cache.get(args[0]); //here we are going to see if the author used id to ban memeber  //here we are going to specify who to ban
+      const person = message.mentions.members.first(); //here we are seeing if the person does specify a person to kick or doesnt
+      if(!person) return message.channel.send('Please ping a person to ban');
 
-      if(!personToban) return message.channel.send('Please specify someone to ban via mention or id');
-
-      if(personToban.id === message.author.id) return message.channel.send(':/ why you want to ban urself bruhhhhhhh')// here we are seeing if the author wasnt to ban himself
-
-      const reason = args.slice(1).join(" "); // here we are seeing if there is a reason to the ban
-
-      await personToban.ban(); // here we are baning person
-
-      if(reason === null) reason = 'No reason';
-
-      message.channel.send(`banned: <@${personToban.id}> \n banned by: <@${message.author.id}>\n reason: ${reason}`)
+      person.ban().then(someone => {
+        message.channel.send(`${someone.user.username} has been banned`)
+      })//here we are kicking the person you can change it a bit like make it send a embed but thats it for this video
     }
 }
--- commands/kick.js
+++ commands/kick.js
@@ -1,27 +1,17 @@
-const Discord = require("discord.js");
-const { MessageEmbed } = require("discord.js");
+const Discord = require('discord.js')
 
 module.exports = {
     name: "kick",
-    description: "kick ppl lol",
+    description: ":/",
 
     async run (client, message, args) {
-      //first we are going to see if the user has perms
-      if(!message.member.hasPermission("KICK_MEMBERS")) return;
-      if(!message.guild.me.hasPermission("KICK_MEMBERS")) return message.channel.send('I don\'t have perms to use this command');// here we are going to check if the bot has perms to kick ppl
+      if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send(':/ you dont have perms to kick this member bruhhhh');
 
-      const personTokick = message.mentions.members.first() || message.guild.members.cache.get(args[0]); //here we are going to see if the author used id to kick memeber  //here we are going to specify who to kick
+      const person = message.mentions.members.first(); //here we are seeing if the person does specify a person to kick or doesnt
+      if(!person) return message.channel.send('Please ping a person to kick');
 
-      if(!personTokick) return message.channel.send('Please specify someone to kick via mention or id');
-
-      if(personTokick.id === message.author.id) return message.channel.send(':/ why you want to kick urself bruhhhhhhh')// here we are seeing if the author wasnt to kick himself
-
-      const reason = args.slice(1).join(" "); // here we are seeing if there is a reason to the kick
-
-      await personTokick.kick(); // here we are kicking person
-
-      if(reason === null) reason = 'No reason';
-
-      message.channel.send(`kicked: <@${personTokick.id}> \n kicked by: <@${message.author.id}>\n reason: ${reason}`)
+      person.kick().then(someone => {
+        message.channel.send(`${someone.user.username} has been kicked`)
+      })//here we are kicking the person
     }
 }
